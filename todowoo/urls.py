b47f076302fe3c7aec4a5edfb08de40b3a057b3d
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-08-22 16:29:03 +0600

    Commit#5 Create Todo App For ToDo Woo

---
+++ todowoo/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+
+    #Auth
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+
+    #Todo
+    path('', views.home, name='home'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('current/', views.currenttodos, name='currenttodos'),
+]
