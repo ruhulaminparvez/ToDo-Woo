88b9492a491dff96461e1a91efb12eebcdf6ab8e
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-08-23 14:14:02 +0600

    Commit#9 Heroku Setup

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,9 +123,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 LOGIN_URL = '/login'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
